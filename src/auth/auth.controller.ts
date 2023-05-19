7bef028414a4884fb2096723f0b02c72b3b8d537
Nicolas <nicolas.bouffanais.1999@gmail.com>
2023-05-18 22:44:18 -0400

    [feature] main.ts app.useGlobalPipes(new ValidationPipe()) to be able to use the pipes for validation in our AuthDto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,28 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
 
     constructor(private authService: AuthService){}
   @Post('/local/signup')
-  signupLocal() {}
+  signupLocal(@Body() dto:AuthDto) {
+    this.authService.signupLocal(dto);
+  }
 
   @Post('/local/signin')
-  signinLocal() {}
+  signinLocal() {
+    this.authService.signinLocal();
+  }
 
   @Post('/logout')
-  logout() {}
+  logout() {
+    this.authService.logout();
+  }
 
   @Post('/refresh')
-  refreshToken() {}
+  refreshToken() {
+    this.authService.refreshToken();
+  }
 }
