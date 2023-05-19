4c4b28fb58f90915f1753510d630cc95f93e3c50
Nicolas <nicolas.bouffanais.1999@gmail.com>
2023-05-18 23:01:55 -0400

    [feature] create 3 files on strategies folder empty

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,24 +2,24 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as bcrypt from 'bcrypt';
+import { Tokens } from './types';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  hashData(data:string){
-    return bcrypt.hash(data,10);
+  hashData(data: string) {
+    return bcrypt.hash(data, 10);
   }
 
-  async signupLocal(dto:AuthDto) {
-    const hash = await this.hashData(dto.password)
+  async signupLocal(dto: AuthDto): Promise<Tokens> {
+    const hash = await this.hashData(dto.password);
     const newUser = this.prisma.user.create({
-        data:{
-            email: dto.email,
-            hash,
-        }
-    })
-    
+      data: {
+        email: dto.email,
+        hash,
+      },
+    });
   }
   signinLocal() {}
   logout() {}
